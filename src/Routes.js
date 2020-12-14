930450cd6f13446f72c4bb0fe8ac1d4dad39e7a6
devCecy <im.ceciliaan@gmail.com>
2020-12-14 16:29:25 +0900

    초기세팅완료

---
+++ src/Routes.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+
+import Cart from './Pages/Cart/Cart';
+import CheckOut from './Pages/CheckOut/CheckOut'
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
+import Main from './Pages/Main/Main';
+import MyPage from './Pages/MyPage/MyPage';
+import ProductDetail from './Pages/ProductDetail/ProductDetail'
+import ProductList from './Pages/ProductList/ProductList'
+
+
+
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/cart' component={Cart} />
+          <Route exact path='/checkout' component={CheckOut} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={SignUp} />
+          <Route exact path='/main' component={Main} />
+          <Route exact path='/mypage' component={MyPage} />
+          <Route exact path='/productdetail' component={ProductDetail} />
+          <Route exact path='/productlist' component={ProductList} />
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default Routes

8258504161f2cec3733af17cb58c7edfb8a63ce8
devCecy <im.ceciliaan@gmail.com>
2020-12-15 00:53:45 +0900

    Add: Login validation & functions

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './Login.scss'
 
+
 class Login extends Component {
   constructor(){
     super();
     this.state = {
       id:'',
       pw:'',
+      idValidation:'',
+      pwValidation:'',
+      keepLogin: false,
+      onOffBtn: false,
     }
   }
 
@@ -27,16 +32,33 @@ class Login extends Component {
     const checkId = this.state.id;
     const checkPw = this.state.pw;
     if (!checkId){
-      alert("아이디를 입력해주세요.")
-    } if (!checkPw){
-      alert("비밀번호를 입력해주세요.")
+      this.setState({ 
+        idValidation : "아이디를 입력해주세요."
+      })
+    } if (!checkPw){ 
+      this.setState({
+        pwValidation : "비밀번호를 입력해주세요."
+      })
     } else {
         this.props.history.push('/')
     }
   }
 
+  handleKeepLogin = () => {
+    this.setState({
+      keepLogin : !this.state.keepLogin
+    })
+  }
+
+  handleOnOffBtn = () => {
+    this.setState({
+      onOffBtn : !this.state.onOffBtn
+    })
+  }
+
 
   render() {
+    console.log(this.state.onOffBtn)
     return (
       <div className="container">
         <header>
@@ -52,9 +74,10 @@ class Login extends Component {
           type="text"
           value={this.state.id}
           onChange={this.handleIdChange}
-          >
-          {/* <span>아이디를 입력해 주세요.</span> */}
-          </input>
+          ></input>
+          <span 
+          className="validationMessage
+          ">{this.state.idValidation}</span>
           <input 
           className="pwBox"
           placeholder="비밀번호"
@@ -63,17 +86,27 @@ class Login extends Component {
           onChange={this.handlePwChange}
           >
           </input>
-          {/* <span>비밀번호를 입력해주세요.</span> */}
+          <span
+          className="validationMessage"
+          >{this.state.pwValidation}</span>
           <button
           className="loginBtn"
           onClick={this.handleValidation}>로그인</button>
           
           <div className="bottomContainer">
-            {/* <img alt="Check Mark" src="/images/check-mark.png" /> */}
-            {/* <i class="far fa-check-circle"></i> */}
-            <span>로그인 상태 유지</span>
-            <span>IP 보안</span>
-            <span>OFF</span>
+            <div className="KeepLoginMark">
+              <i 
+              className={this.state.keepLogin ? "fas fa-check-circle" : "far fa-check-circle"}
+              onClick={this.handleKeepLogin}
+              ></i>
+              <span>로그인 상태 유지</span>
+            </div>
+            <div className="ipSecurity">
+              <span>IP 보안</span>
+              <span 
+              className={this.state.onOffBtn ? "onBtn" : "offBtn"}
+              onClick={this.handleOnOffBtn}>{this.state.onOffBtn ? "ON" : "OFF"}</span>
+            </div>
           </div>
         </div>
       </div>

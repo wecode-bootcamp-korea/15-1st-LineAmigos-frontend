48d25cf362185a4a28d482f8ebf5546799004b42
Eunjin Lee <reginalee@Eunjins-MacBook-Pro-8.local>
2020-12-15 18:03:04 +0900

    Main slider 수정중

--- src/Pages/Main/MainSlider.jsx
+++ src/Pages/Main/MainSlider.jsx
@@ -1,26 +1,29 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
+import './MainSlider.scss'
 
 export default class MainSlider extends Component {
   render() {
     const settings = {
-      dots: true,
+      dots: false,
       fade: true,
       infinite: true,
-      speed: 500,
+      speed: 700,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
     };
 
     const images = [
       {id: 1, url:'/images/Main/main-slider-01.jpg'},
-      {id: 1, url:'/images/Main/main-slider-02.jpg'},
-      {id: 1, url:'/images/Main/main-slider-03.jpg'},
-      {id: 1, url:'/images/Main/main-slider-04.jpg'},
-      {id: 1, url:'/images/Main/main-slider-05.jpg'},
-      {id: 1, url:'/images/Main/main-slider-06.jpg'},
+      {id: 2, url:'/images/Main/main-slider-02.jpg'},
+      {id: 3, url:'/images/Main/main-slider-03.jpg'},
+      {id: 4, url:'/images/Main/main-slider-04.jpg'},
+      {id: 5, url:'/images/Main/main-slider-05.jpg'},
+      {id: 6, url:'/images/Main/main-slider-06.jpg'},
     ]
-    
+
     return (
       <div className="MainSlider">
         <Slider {...settings}>

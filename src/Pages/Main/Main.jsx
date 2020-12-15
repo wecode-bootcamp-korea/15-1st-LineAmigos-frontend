65a73b55213f9bbb3a34d3c9bbea2fe5aa7127f7
Eunjin Lee <reginalee@Eunjins-MacBook-Pro-8.local>
2020-12-15 18:42:25 +0900

    메인페이지 상품리스트 작업중

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -14,8 +14,18 @@ class Main extends React.Component {
           <MainSlider />
         </div>
         <Header />
-        <div className="middleContainer">
-        
+        <div className="newProducts">
+          <h3>새로 나왔어요</h3>
+          <div className="newProductContainer">
+            <img alt="Product" src="/images/product.jpg" />
+            <div className="productInfo">
+              <span className="saleRate">40%</span>
+              <span className="price">63,900원</span>
+            </div>
+            <span className="productInfo">`(12/14일 출시)라인프렌즈 브롤스타즈 시로의 베스트셀러 에디션`
+            </span>
+          </div>
+
         </div>
         <Footer />
       </div>

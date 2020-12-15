9494729c7b4d97069ef45aeb5297a29574a313f0
Eunjin Lee <reginalee@Eunjins-MacBook-Pro-8.local>
2020-12-15 12:17:06 +0900

    footer 시작

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from'react';
+import ReactDOM from 'react-dom';
+import Routes from './Routes'
+import './styles/reset.scss'
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/Pages/Main/Main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+// import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
+import './Main.scss'
+
+class Main extends React.Component {
+
+  render() {
+    return (
+      <>
+        <Header />
+        <div className="visualContainer">
+          <div className="visualImages">
+            <img alt="Slider image" className="sliderImage"/>
+            <img alt="Slider image" className="sliderImage"/>
+            <img alt="Slider image" className="sliderImage"/>
+          </div>
+          <div className="visualText">
+            <img alt="Slider text" className="sliderText"/>
+            <img alt="Slider text" className="sliderText"/>
+            <img alt="Slider text" className="sliderText"/>
+          </div>
+          <div className="arrowContainer">
+            <img alt="arrow" />
+          </div>
+        </div>
+    </>
+    )
+  }
+}
+
+export default Main

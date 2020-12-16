d55240f0bba28d23f968c5989a5651be51647dc6
Eunjin Lee <reginalee@Eunjins-MacBook-Pro-8.local>
2020-12-16 16:41:20 +0900

    Remove: Footer와 관련 없는 Main 컴포넌트 내용 삭제

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -7,21 +7,6 @@ class Main extends React.Component {
   render() {
     return (
       <>
-        <div className="visualContainer">
-          <div className="visualImages">
-            <img alt="Slider image" className="sliderImage"/>
-            <img alt="Slider image" className="sliderImage"/>
-            <img alt="Slider image" className="sliderImage"/>
-          </div>
-          <div className="visualText">
-            <img alt="Slider text" className="sliderText"/>
-            <img alt="Slider text" className="sliderText"/>
-            <img alt="Slider text" className="sliderText"/>
-          </div>
-          <div className="arrowContainer">
-            <img alt="arrow" />
-          </div>
-        </div>
         <Footer />
       </>
     )

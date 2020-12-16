e2e76662bf330ba0405692e5b6b8b66a440cac12
devCecy <im.ceciliaan@gmail.com>
2020-12-16 20:53:55 +0900

    Code Refactoring Complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -14,9 +14,29 @@ class Login extends Component {
     }
   }
 
-  handleIdChange = (event) => {
+  //API 받을 준비
+  handleClick = () => {
+    fetch('API주소', {
+      method: 'POST',
+      body: JSON.stringify({
+        userId: this.state.id, //승현님이 정한 key명 확인해서 변경해야함!
+        password: this.state.pw, //승현님이 정한 key명 확인해서 변경해야함!
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        //제3의 이름이 아닌 위의 인자와 같은 이름을 써도 되는 이유 질문하기
+        if (result.Authorization) {
+          localStorage.setItem('token', result.Authorization)
+          this.props.history.push('/')
+        }
+      })
+  }
+
+  handleIdPwChange = (event) => {
+    const { id, value } = event.target
     this.setState({
-      id: event.target.value,
+      [id]: value,
     })
   }
 
@@ -32,12 +52,11 @@ class Login extends Component {
     const checkPw = this.state.pw
     this.setState({
       idValidation: !checkId ? '아이디를 입력해주세요.' : '',
-    })
-    this.setState({
       pwValidation: !checkPw ? '비밀번호를 입력해주세요.' : '',
     })
     if (checkId && checkPw) {
-      this.props.history.push('/')
+      // this.handleClick() // api 연결하면 살릴 부분
+      this.props.history.push('/') // api연결하면 지워야하는 부분
     }
   }
 
@@ -54,7 +73,14 @@ class Login extends Component {
   }
 
   render() {
-    console.log(this.state.onOffBtn)
+    const {
+      id,
+      pw,
+      idValidation,
+      pwValidation,
+      keepLogin,
+      onOffBtn,
+    } = this.state
     return (
       <div className='container'>
         <header>
@@ -66,26 +92,28 @@ class Login extends Component {
         </header>
         <div className='loginForm'>
           <input
+            id='id'
             className='idBox'
             placeholder='아이디'
             type='text'
-            value={this.state.id}
-            onChange={this.handleIdChange}
-          ></input>
+            value={id}
+            onChange={this.handleIdPwChange}
+          />
           <span
             className='validationMessage
           '
           >
-            {this.state.idValidation}
+            {idValidation}
           </span>
           <input
+            id='pw'
             className='pwBox'
             placeholder='비밀번호'
             type='password'
-            value={this.state.pw}
-            onChange={this.handlePwChange}
-          ></input>
-          <span className='validationMessage'>{this.state.pwValidation}</span>
+            value={pw}
+            onChange={this.handleIdPwChange}
+          />
+          <span className='validationMessage'>{pwValidation}</span>
           <button className='loginBtn' onClick={this.handleValidation}>
             로그인
           </button>
@@ -93,22 +121,20 @@ class Login extends Component {
           <div className='bottomContainer'>
             <div className='KeepLoginMark'>
               <i
-                className={
-                  this.state.keepLogin
-                    ? 'fas fa-check-circle'
-                    : 'far fa-check-circle'
-                }
+                className={`fa-check-circle ${
+                  this.state.keepLogin ? 'fas' : 'far'
+                }`}
                 onClick={this.handleKeepLogin}
-              ></i>
+              />
               <span>로그인 상태 유지</span>
             </div>
             <div className='ipSecurity'>
               <span>IP 보안</span>
               <span
-                className={this.state.onOffBtn ? 'onBtn' : 'offBtn'}
+                className={onOffBtn ? 'onBtn' : 'offBtn'}
                 onClick={this.handleOnOffBtn}
               >
-                {this.state.onOffBtn ? 'ON' : 'OFF'}
+                {onOffBtn ? 'ON' : 'OFF'}
               </span>
             </div>
           </div>

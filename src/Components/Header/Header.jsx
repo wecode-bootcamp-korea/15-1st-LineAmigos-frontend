09632956828b1cb5fc8655ea500ed1d6aef6c08d
Eunjin Lee <reginalee@Eunjins-MacBook-Pro-8.local>
2020-12-17 13:41:07 +0900

    Add: Header 컴포넌트 검색기능 구현

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,22 +6,38 @@ class Header extends React.Component {
     super()
     this.state = {
       categories: [],
+      searchValue : '',
+      searchList: [],
     }
   }
   
+  handleSearchValue = (e) => {
+    this.setState({
+      searchValue: e.target.value,
+    })
+  }
+
   componentDidMount = () => {
     fetch('/data/productsInfos.json')
       .then(response => response.json())
       .then(data => {
-        // console.log(data)
         this.setState({
-          categories: data.navCategories
+          categories: data.navCategories,
+          searchList: data.products,
         })
       }).catch(err => console.log(err))
   }
 
   render() {
-    const {categories} = this.state
+
+    const {categories, searchValue, searchList} = this.state
+    const filteredList = searchList.filter((product) => {
+      if (product.productName.toLowerCase().includes(searchValue.toLowerCase())) {
+        return product
+      }
+    })
+    console.log(filteredList)
+
     return (
       <header className='Header'>
         <div className="innerContainer">
@@ -43,25 +59,47 @@ class Header extends React.Component {
               <img alt="Line Amigos Logo" src="/images/line-amigos-logo-black.png"/>
             </h1>
             <div className="searchContainer">
-              <input type="text" placeholder="검색어를 입력해보세요"></input>
-              <img alt="Search Icon" src="/images/search.png"/>
+              <input 
+                type="text" 
+                placeholder="검색어를 입력해보세요"
+                onChange={this.handleSearchValue} />
+              <img alt="Search Icon" src="/images/search.png" className="searchIcon"/>
+              <ul className={!searchValue || filteredList.length === 0 ? "searchListContainer " : "searchListContainer open"}>
+                {filteredList &&
+                  filteredList.map(item => {
+                    return(
+                      <li key={item.id}>
+                        <img alt={item.productName} src={item.url} className="itemImg" />
+                        <div>
+                          <span className="productName">{item.productName}</span>
+                          <span className="price">{item.price}원</span>
+                        </div>
+                      </li>
+                    )
+                  })
+                }
+              </ul>
             </div>
           </div>
           <ul className="topMenu">
-            {/*{
-              categories.map((category, index) => {
+            {categories &&
+              categories.map((navCategory, index) => {
                 return (
                   <li key={index}>
-                    <div>{category}</div>
+                    <div>{navCategory.category}</div>
                     <img alt="Down arrow" src="/images/arrow-right-bold.png" />
                   </li>
                 )
               })
-            }*/}
+            }
           </ul>
           <div className="navDropdown">
             <ul>
-              <li></li>
+              <li>subcategory1</li>
+              <li>subcategory2</li>
+              <li>subcategory3</li>
+              <li>subcategory4</li>
+              <li>subcategory5</li>
             </ul>
           </div>
         </div>

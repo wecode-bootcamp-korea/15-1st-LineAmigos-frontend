19901789d36bfd9097a83c332cae2d67663ff03a
Eunjin Lee <reginalee@Eunjins-MacBook-Pro-8.local>
2020-12-20 12:43:22 +0900

    Add: 장바구니 버튼 및 기능 mockdata로 구현, UI는 아직

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
 import './Cart.scss';
 
 class Cart extends React.Component {
@@ -6,88 +7,138 @@ class Cart extends React.Component {
   constructor() {
     super()
     this.state = {
-      cartItem: []
+      cartItems: [],
+      isAllSelected: false,
     }
   }
 
-  addItem = () => {
-    // fetch('API', {
-    //   method: 'PATCH',
-    //   body: {
-    //     item_id: itemId,
-    //   }
-    // })
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     this.setState({
-    //       cartItem: data.cartItems
-    //     })
-    //   })
+  //getting modified cart data from backend
+  getCartData = () => {
+    fetch('/data/productsInfos.json')
+    // fetch('http://10.58.4.1:8000/cart', { //session example
+      .then(res => res.json())
+      .then(res => {
+        if (res.MESSAGE === "SUCCESS") {
+          this.setState({
+            cartItems: res.cartItems
+          })
+        }
+      }).catch(err => console.log(err))
   }
 
-  subtractItem = () => {
-    // fetch('API', {
-    //   method: 'PATCH',
-    //   body: {
-    //     item_id: itemId,
-    //   }
-    // })
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     this.setState({
-    //       cartItem: data.cartItems
-    //     })
-    //   })
+  //click to add item amount one by one 
+  addCartItem = () => {
+    fetch('API', {
+    // fetch('http://10.58.4.1:8000/cart', { //session example
+      method: 'PATCH',
+      body: JSON.strinify({
+        // item_id: itemId
+      })
+    }).then(res => res.json())
+      .then(res => res.MESSAGE === "SUCCESS" &&
+        this.setState({cartItem: res.cartItems}))
+      .catch(err => console.log(err))
   }
 
-  deleteItem = () => {
-    // fetch('API', {
-    //   method: 'DELETE',
-    //   body: {
-    //     item_id: itemId,
-    //   }
-    // })
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     this.setState({
-    //       cartItem: data.cartItems
-    //     })
-    //   })
+  //click to subtract item amount one by one
+  subtractCartItem = () => {
+    fetch('API', {
+    // fetch('http://10.58.4.1:8000/cart', { //session example
+      method: 'PATCH',
+      body: JSON.strinify({
+        // item_id: itemId
+      })
+    }).then(res => res.json())
+      .then(res => res.MESSAGE === "SUCCESS" &&
+        this.setState({cartItem: res.cartItems}))
+      .catch(err => console.log(err))
   }
 
-  getCartItems = () => {
-    // fetch('API', {
-    //   headers: {
-    //     Authorization: localStorage.getItem("token")
-    //   }
-    // })
-    //   .then(response => response.json)
-    //   .then(data => {
-    //     this.setState({
-    //       cartItem: data.cartItem
-    //     })
-    //   })
+  //delete button click
+  deleteCartItem = () => {
+    fetch('API', {
+    // fetch('http://10.58.4.1:8000/cart', { //session example
+      method: 'DELETE',
+      body: JSON.strinify({
+        // item_id: itemId
+      })
+    }).then(res => res.json())
+      .then(res => res.MESSAGE === "SUCCESS" && this.getCartItems())
+      .catch(err => console.log(err))
   }
 
+  //delete selected items => map selected items and deleteCartItem func 
+  deleteSelectedCartItems = () => {
+    const { cartItems } = this.state
+    const selectedItems = cartItems.filter(cartItem => cartItem.isChecked)
+    selectedItems.forEach(item => {
+        fetch('API', {
+        // fetch('http://10.58.4.1:8000/cart', { //session example
+          method: 'DELETE',
+          body: JSON.strinify({
+            item_id: item.id
+          })
+        }).then(res => res.json())
+          .then(res => res.MESSAGE === "SUCCESS" && this.getCartItems())
+          .catch(err => console.log(err))
+      }
+    )
+  }
+
+  //select cart items
+  handleSelectCartItem = (e) => {
+    const { id } = e.target.id
+    const { cartItems } = this.state 
+    const newSelectedStatus = cartItems.map(cartItem => {
+      if (cartItem.productId === id) {
+        cartItem.isChecked = !this.state.cartItem.isChecked
+      }
+      return cartItem
+    })
+    this.setState({cartItems: newSelectedStatus})
+  }
+
+  //select all items
+  handleSelectAllCartItems = () => {
+    const { cartItems } = this.state
+    const allSeletedStatus = cartItems.forEach(cartItem => {
+      cartItem.isChecked = true
+    })
+    this.setState({cartItems: allSeletedStatus})
+  }
+
+  //get cart data after rendered
   componentDidMount = () => {
-    // fetch('/data/productsInfos.json')
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     this.setState({
-    //       cartItem: data.products
-    //     })
-    //   })
+    //this.getCartData()
+    fetch('/data/productsInfos.json')
+      .then(res => res.json())
+      .then(data => this.setState({cartItems: data.cartItems}))
   }
+
+  //go to product detail page
+  goProductDetailPage = (e) => {
+    const { id } = e.target.id
+    const { cartItems } = this.state
+    const targetId = cartItems.filter(cartItem => cartItem.productId === id)
+    this.props.history.push(`/product/${targetId}`)
+  }
+
+  backToShoppingPage = () => {this.props.history.goBack()}
+  goToCheckOutPage = () => {this.props.history.push("/checkout")}
 
   render() {
-    const { addProduct, subtractProduct } = this
+    const { addItem, subtractItem, deleteItem } = this
+    const { cartItems } = this.state
+    const selectedItems = cartItems.filter(cartItem => cartItem.isChecked)
+    const totalPrice = selectedItems.map(cartItem => cartItem.price).reduce((a, b) => a + b, 0)
+    const discountPrice = selectedItems.reduce((a, item) => a + item.price*item.saleRate*0.01, 0)
+    const checkOutPrice = totalPrice - discountPrice
+    const unavailable = <div className="unavailable">구매불가</div>
     const NOTICE = [
       "장바구니 상품은 최대 30일간 저장됩니다.",
       "가격, 옵션 등 정보가 변경된 경우 주문이 불가할 수 있습니다.",
       "오늘출발 상품은 판매자 설정 시점에 따라 오늘출발 여부가 변경될 수 있으니 주문 시 꼭 다시 확인해 주시기 바랍니다.",
     ]
-    const unavailable = <div className="unavailable">구매불가</div>
-    // const totalPrice = 
 
     return (
       <div className="Cart">
@@ -110,83 +161,86 @@ class Cart extends React.Component {
               {
                 NOTICE.map((el, index) => {
                   return (
-                    <li key={index}><i class="fas fa-circle"/>{el}</li>
+                    <li key={index}><i className="fas fa-circle"/>{el}</li>
                   )
                 })
               }
             </div>
           </header>
-          <div className="row title">
-            <div className="checkbox"><input type="checkbox" /></div>
-            <div className="detail"></div>
-            <div className="options"></div>
-            <div className="price"></div>
-          </div>
-          <ul>
-            {
-              this.state.cartItem.map((item, index) => {
-                return(
-                  <li key={index} className="row items">
-                    <div className="checkbox"><input type="checkbox" /></div>
-                    <div className="productDetail">
-                      <img alt={item.name} src={item.image_url} />
-                      <div className="detail">
-                        <div className="store">라인아미고스</div>
-                        <div className="storeCategory">스마트스토어</div>
+          <div className="cartItemList">
+            <div className="row title">
+              <div className="checkbox"><input type="checkbox" /></div>
+              <div className="productDetail"></div>
+              <div className="options"></div>
+              <div className="priceInfo"></div>
+            </div>
+            <ul>
+              {
+                cartItems.map((item, index) => {
+                  return(
+                    <li key={index} className="row items">
+                      <div className="checkbox"><input type="checkbox" /></div>
+                      <div className="productDetail">
+                        <img alt={item.name} src={item.image_url} />
+                        <div className="detailBox">
+                          <div className="detail">라인아미고스</div>
+                          <div className="productName">{item.name}</div>
+                          <div className="price">{item.price}원</div>
+                          <i 
+                            className="fas fa-times"
+                            onClick={deleteItem}></i>
+                        </div>
+                     
                       </div>
-                      <div className="productName">{item.name}</div>
-                      <div className="price">{item.price}원</div>
-                      <i class="fas fa-times"></i>
-                    </div>
-                    <div className="options">
-                      <div className="option">사이즈: 단품</div>
-                      <div className="modify">
-                        <span 
-                          className="plus" 
-                          onClick={addProduct}/> 
-                        <span>{item.quantity}</span> 
-                        <span 
-                          className="subtract" 
-                          onClick={subtractProduct}/>
+                      <div className="options">
+                        <div className="option">사이즈: 단품</div>
+                        <div className="modify">
+                          <span 
+                            className="plus" 
+                            onClick={addItem}/> 
+                          <span>{item.stock}</span> 
+                          <span 
+                            className="subtract" 
+                            onClick={subtractItem}/>
+                        </div>
                       </div>
-                    </div>
-                    <div className="priceInfo">
-                      <div className="price">{!item.stock && {unavailable}}{item.price}원</div>
-                      <div className="order">주문하기</div>
-                    </div>
-                  </li>
-                )
-              })
-            }
-            </ul>
-            <div className="row selectActions">
-              <div className="checkbox"><checkbox /></div>
-              <div className="buttons">
-                <div className="delete"></div>
-                <div className="addToWishList"></div>
+                      <div className="priceInfo">
+                        <div className="price">{!item.stock && {unavailable}}{item.price}원</div>
+                        <div className="order">주문하기</div>
+                      </div>
+                    </li>
+                  )
+                })
+              }
+              </ul>
+              <div className="row selectActions">
+                <div className="checkbox"><input type="checkbox" /></div>
+                <div className="buttons">
+                  <div className="delete"></div>
+                  <div className="addToWishList"></div>
+                </div>
               </div>
             </div>
-
             <div className="calculation">
               <div className="summary">
                 <div className="totalPrice">
                   <div className="title">총 상품금액</div>
-                  <div className="total">{}원</div>
+                  <div className="total">{totalPrice}원</div>
                 </div>
-                <i class="fas fa-plus"></i>
+                <i className="fas fa-plus" />
                 <div className="shippingFee">
                   <div className="title">배송비</div>
-                  <div className="Fee">{}원</div>
+                  <div className="Fee">{checkOutPrice > 30000 ? 0 : 3000}원</div>
                 </div>
-                <i class="fas fa-minus"></i>
+                <i className="fas fa-minus" />
                 <div className="discounted">
                   <div className="title">총 할인예상금액</div>
-                  <div className="discountPrice">{}원</div>
+                  <div className="discountPrice">{discountPrice}원</div>
                 </div>
               </div>
-              <div className="totalAmount">
+              <div className="checkOutPrice">
                 <span>총 주문금액</span>
-                <span>{}원</span>
+                <span>{checkOutPrice}원</span>
               </div>
             </div>
             <div className="buttons">
@@ -199,4 +253,4 @@ class Cart extends React.Component {
   }
 }
 
-export default Cart;
+export default withRouter(Cart);
--- src/Routes.js
+++ src/Routes.js
@@ -7,12 +7,12 @@ import {
 
 import Main from './Pages/Main/Main';
 import Cart from './Pages/Cart/Cart';
-import CheckOut from './Pages/CheckOut/CheckOut'
-import Login from './Pages/Login/Login';
-import SignUp from './Pages/SignUp/SignUp';
-import MyPage from './Pages/MyPage/MyPage';
-import ProductDetail from './Pages/ProductDetail/ProductDetail'
-import ProductList from './Pages/ProductList/ProductList'
+// import CheckOut from './Pages/CheckOut/CheckOut'
+// import Login from './Pages/Login/Login';
+// import SignUp from './Pages/SignUp/SignUp';
+// import MyPage from './Pages/MyPage/MyPage';
+// import ProductDetail from './Pages/ProductDetail/ProductDetail'
+// import ProductList from './Pages/ProductList/ProductList'
 
 class Routes extends React.Component {
   render() {
@@ -21,12 +21,12 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path='/' component={Main} />
           <Route exact path='/cart' component={Cart} />
-          <Route exact path='/checkout' component={CheckOut} />
+{/*          <Route exact path='/checkout' component={CheckOut} />
           <Route exact path='/login' component={Login} />
           <Route exact path='/signup' component={SignUp} />
           <Route exact path='/mypage' component={MyPage} />
           <Route exact path='/productdetail' component={ProductDetail} />
-          <Route exact path='/productlist' component={ProductList} />
+    <Route exact path='/productlist' component={ProductList} />*/}
         </Switch>
       </Router>
     )

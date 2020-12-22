60fbc41e4cd234d86db05fdd97036eb9fd4e8298
Eunjin Lee <reginalee@Eunjins-MacBook-Pro-8.local>
2020-12-22 14:38:10 +0900

    Add: config.js 미현님 경오님 mock data  추가

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 
-// ==================================== local mock data (eunjin) 
+// local mock data (eunjin) 
 export const PRODUCTS_INFO = "./public/data/productsInfos.json"
+// local mock data (mihyun) 
+export const FILTER_DATA = "./public/data/filterData.json"
 
-// ==================================== API from backend  
+// API from backend  
 export const CATEGORY_API = "http://[호스트]/product/menu";
 export const ALLPRODUCTS_API = "http://[호스트]/product/products_info";
 export const DETAIL_API = "http://[호스트]/product/[상품ID]";

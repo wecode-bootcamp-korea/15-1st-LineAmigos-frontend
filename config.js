47902e6b17b94661a658731afb2a8f1b7cbdcb1e
Eunjin Lee <reginalee@Eunjins-MacBook-Pro-8.local>
2020-12-22 14:40:39 +0900

    Fix: config.js 수정

--- config.js
+++ config.js
@@ -3,6 +3,9 @@
 export const PRODUCTS_INFO = "./public/data/productsInfos.json"
 // local mock data (mihyun) 
 export const FILTER_DATA = "./public/data/filterData.json"
+// local mock data (kyungoh) 
+export const REVIEW_DATA = "./public/data/reviewData.json"
+export const MOCK_DATA="./public/data/mockData.json"
 
 // API from backend  
 export const CATEGORY_API = "http://[호스트]/product/menu";

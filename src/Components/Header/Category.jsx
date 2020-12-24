1364a30050595e47592a15f19e409cb7cabc52ee
devCecy <im.ceciliaan@gmail.com>
2020-12-24 16:36:23 +0900

    Project Completed

--- src/Components/Header/Category.jsx
+++ src/Components/Header/Category.jsx
@@ -1,34 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 import './Category.scss'
 
 class Category extends React.Component {
-
   render() {
     const { category } = this.props
     return (
-      <div className="Category">
+      <div className='Category'>
         <li>
-          <div className="categoryItem">{category.menu}</div>
-          <img 
-            alt="Down arrow" 
-            src="/images/arrow-right-bold.png" 
-            className={category.categories.length>1 ? 'show' : 'hide'}/>
-          <div className={category.categories.length>1 ? "navDropdown" : "navDropdown preventHover"}>
-            <ul className="subCategories">
+          <div className='categoryItem'>{category.menu}</div>
+          <img
+            alt='Down arrow'
+            src='/images/arrow-right-bold.png'
+            className={category.categories.length > 1 ? 'show' : 'hide'}
+          />
+          <div
+            className={
+              category.categories.length > 1
+                ? 'navDropdown'
+                : 'navDropdown preventHover'
+            }
+          >
+            <ul className='subCategories'>
               {category.categories.length > 0 &&
                 category.categories.map((subCategory, index) => {
                   return (
-                    <li key={index}><Link to="/product/products_info?limit=20" className="reset">{subCategory}</Link></li>
+                    <li key={index}>
+                      <Link to='/productlist' className='reset'>
+                        {subCategory}
+                      </Link>
+                    </li>
                   )
-                })
-              }
+                })}
             </ul>
           </div>
         </li>
       </div>
-    );
+    )
   }
 }
 
-export default Category;
+export default Category
